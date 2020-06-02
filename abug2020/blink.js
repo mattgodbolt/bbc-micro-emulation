d579f3d4c6a715df4bee5fbf83954befd37003b0
Matt Godbolt <matt@godbolt.org>
2020-06-01 21:32:16 -0500

    ABUG stuff

---
+++ abug2020/blink.js
@@ -0,0 +1,38 @@
+(() => {
+    setInterval(() => {
+        for (let cursor of document.querySelectorAll("span.cursor")) {
+            cursor.classList.toggle("blink");
+        }
+    }, 600);
+
+    function startTyping(event) {
+        const toReveal = [];
+        const cursor = document.createElement("span");
+        cursor.classList.add("cursor");
+        for (let f of event.querySelectorAll(".type-in")) {
+            toReveal.push(() => {
+                f.appendChild(cursor);
+            });
+            for (let child of f.children) {
+                if (child.tagName === "SPAN") {
+                    for (let char of child.textContent) {
+                        toReveal.push(() => child.textContent += char);
+                    }
+                    child.innerText = "";
+                } else if (child.tagName === "BR") {
+                    child.classList.add("hidden");
+                    toReveal.push(() => child.classList.remove("hidden"));
+                }
+            }
+        }
+        const next = () => {
+            if (!toReveal.length) return;
+            toReveal[0]();
+            toReveal.shift();
+            setTimeout(next, 50 + 120 * Math.random());
+        };
+        setTimeout(next, 200);
+    }
+
+    Reveal.addEventListener("slidechanged", (event) => startTyping(event.currentSlide));
+})();
---
+++ abug2020/notes.md
@@ -0,0 +1,43 @@
+abug
+----
+
+* Who am I?
+  * Live in Chicago now
+  * Better known for C++, optimization, Compiler Explorer
+* Why Beeb?
+  * RTW
+    * best friend since we were 12
+    * Hacked on Acorn User stuff together
+    * Original author of BeebAsm
+    * now works at Epic on Unreal stuff
+...
+
+
+Demo?
+```js
+utils = require('utils')
+utils.hexbyte(24)
+```
+
+Break on read (or write)
+```js
+processor.debugRead.add(addr => addr === 0xe00)
+```
+
+Crappy CPU profiler:
+```js
+addrBits = 4
+addrHistogram = new Array(1<<(16-addrBits)).fill(0);
+processor.debugInstruction.add(addr => {addrHistogram[addr >>> addrBits]++; return false; });
+addrHistogram.map((count, addr) => { return {count, addr: utils.hexword(addr<<addrBits)}; }).sort((x,y) => y.count - x.count)
+```
+
+Opcode histogram:
+```js
+histogram = new Array(256).fill(0);
+processor.debugInstruction.add((_, opcode) => {histogram[opcode]++; return false; });
+histogram.map((count, opcode) => { return {count, opcode}; }).sort((x,y) => y.count - x.count)
+```
+
+* tracing
+* debugread
