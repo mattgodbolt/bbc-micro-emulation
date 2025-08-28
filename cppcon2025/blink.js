f0aa5fb4ea98187300da5879237e35e9e9edb5ba
Matt Godbolt <matt@godbolt.org>
2025-08-28 18:41:57 -0500

    Maybe some fixups

--- cppcon2025/blink.js
+++ cppcon2025/blink.js
@@ -1,41 +1,36 @@
-setInterval(() => {
-    for (let cursor of document.querySelectorAll("span.cursor")) {
-        cursor.classList.toggle("blink");
-    }
-}, 600);
+import Reveal from "./reveal.js/dist/reveal.esm.js";
 
 function startTyping(event) {
-    const toReveal = [];
-    const cursor = document.createElement("span");
-    cursor.classList.add("cursor");
-    for (let f of event.querySelectorAll(".type-in")) {
-        toReveal.push(() => {
-            f.appendChild(cursor);
-        });
-        for (let child of f.children) {
-            if (child.tagName === "SPAN") {
-                const originalText = child.textContent;
-                child.innerText = "";
-                child.style.visibility = "visible";
-                for (let char of originalText) {
-                    toReveal.push(() => child.textContent += char);
-                }
-            } else if (child.tagName === "BR") {
-                child.classList.add("hidden");
-                toReveal.push(() => child.classList.remove("hidden"));
-            }
+  const toReveal = [];
+  const cursor = document.createElement("span");
+  cursor.classList.add("cursor");
+  for (let f of event.querySelectorAll(".type-in")) {
+    toReveal.push(() => {
+      f.appendChild(cursor);
+    });
+    for (let child of f.children) {
+      if (child.tagName === "SPAN") {
+        const originalText = child.textContent;
+        child.innerText = "";
+        child.style.visibility = "visible";
+        for (let char of originalText) {
+          toReveal.push(() => (child.textContent += char));
         }
+      } else if (child.tagName === "BR") {
+        child.classList.add("hidden");
+        toReveal.push(() => child.classList.remove("hidden"));
+      }
     }
-    const next = () => {
-        if (!toReveal.length) return;
-        toReveal[0]();
-        toReveal.shift();
-        setTimeout(next, 50 + 120 * Math.random());
-    };
-    setTimeout(next, 200);
+  }
+  const next = () => {
+    const toRun = toReveal.shift();
+    if (!toRun) return;
+    toRun();
+    setTimeout(next, 30 + 90 * Math.random());
+  };
+  setTimeout(next, 200);
 }
 
 export function initializeBlink() {
-    Reveal.on("ready", (event) => startTyping(event.currentSlide));
-    Reveal.on("slidechanged", (event) => startTyping(event.currentSlide));
+  Reveal.on("slidechanged", (event) => startTyping(event.currentSlide));
 }
